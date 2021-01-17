891fd8e70aea13a4a7445c2426cbc48edd33c6ea
Vitalii <testhome@home.home>
2021-01-17 19:19:08 +0200

    Первое задание. Список фильмов

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-console.log('Hello');
+const numberOfFFilms = +prompt('Сколько фильмов ты уже посмотрел?', '');
 
-const miger = 1;
+const personalMovieDB = {
+    count: numberOfFFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let a = prompt('Один из просмотренных фильмов', ''),
+    b = prompt('Оцени его по шкале от 1 до 10',''),
+    c = prompt('Один из просмотренных фильмов', ''),
+    d = prompt('Оцени его по шкале от 1 до 10','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
