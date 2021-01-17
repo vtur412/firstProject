0da81ddbe2c9fd5a414b5504ef842d3720421b9b
Vitalii <testhome@home.home>
2021-01-17 20:20:56 +0200

    Мод скрипта. Валидация промптов. Добавление цикла

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 const numberOfFFilms = +prompt('Сколько фильмов ты уже посмотрел?', '');
 
 const personalMovieDB = {
@@ -8,10 +9,28 @@ const personalMovieDB = {
     privat: false
 };
 
-let a = prompt('Один из просмотренных фильмов', ''),
-    b = prompt('Оцени его по шкале от 1 до 10',''),
-    c = prompt('Один из просмотренных фильмов', ''),
-    d = prompt('Оцени его по шкале от 1 до 10','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+
+for(let i = 0; i < 2; i++){
+    const a = prompt('Один из просмотренных фильмов', ''),
+          b = prompt('Оцени его по шкале от 1 до 10','');
+    
+    if(a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log ('done');
+    }else{
+       console.log('error');
+       i--;
+    }
+}
+
+if(personalMovieDB.count < 10){
+    console.log('Просмотренно мало фильмов');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('Вы классический зритель');
+}else if(personalMovieDB.count >= 30){
+    console.log('Вы киноман!');
+}else{console.log('Произошла ошибка');}
+
+console.log(personalMovieDB);
